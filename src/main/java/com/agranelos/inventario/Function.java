a4b7b8bd13d7875a747e6a7260a0426c57732c45
Diego Pablo Barros Araya <75908052+DiegoBarrosA@users.noreply.github.com>
2025-08-31 16:39:12 -0400

    Update Function.java

--- src/main/java/com/agranelos/inventario/Function.java
+++ src/main/java/com/agranelos/inventario/Function.java
@@ -12,6 +12,7 @@ import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 import java.math.BigDecimal;
